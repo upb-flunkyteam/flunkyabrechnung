e8823f0709d4cd1f24dc3a553825d39c638102ce
Stefan Heid <sheid@mail.uni-paderborn.de>
2020-02-26 14:19:04 +0100

    Revert "Merge branch 'list-predictor' into 'master'"
    This reverts merge request !1

--- nextFlunkerPredictor/read_tally_df.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from sqlalchemy import create_engine
-
-engine = create_engine('sqlite:///flunky.db')
-with engine.connect() as conn, conn.begin():
-    data = pd.read_sql_table('tallymarks', conn)
-print(data.describe())
-data.to_csv('tally.csv')
